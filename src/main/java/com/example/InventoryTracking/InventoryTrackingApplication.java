b04d041c7b0e36608810d0e900aeff45f7a80011
Joel <joe.ernest9305@gmail.com>
2023-06-05 15:34:54 -0500

    initial commit

---
+++ src/main/java/com/example/InventoryTracking/InventoryTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.InventoryTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InventoryTracking/InventoryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InventoryTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
